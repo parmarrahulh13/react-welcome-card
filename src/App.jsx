cefb04176810882c825244cc8137d32bc6de4c50
Rahul <parmarrahulh13@gmail.com>
2026-07-28 22:39:34 +0530

    learn folder structre && how to implement css,js

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,16 @@ import './App.css'
 
 function App() {
   
+  let name = "Rahul";
+  function greet(){
+    alert("this is alert");
+  }
 
   return (
     <>
      <div>
-      <h1><i>hello this my first project in react</i></h1>
+      <h1><i>hello this my first project in react i'm {name}</i></h1>
+      <button onClick={greet}>click me</button>
       <Navbar/>
      </div>
     </>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,7 @@
+import "./Navbar.css";
+
 function Navbar(){
-    return <h1>hello this is component</h1>;
+    return <h1 className="nav">hello this is component</h1>;
 };
 
 export default Navbar;
