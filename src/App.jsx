5f66d905983df94032f3bcf05bcf71894f151514
Rahul <parmarrahulh13@gmail.com>
2026-07-29 19:27:34 +0530

    working on welcome card website created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,16 @@ import heroImg from './assets/hero.png'
 import Navbar from './Navbar'
 import './App.css'
 
+
 function App() {
   
-  let name = "Rahul";
-  function greet(){
-    alert("this is alert");
-  }
+  
 
   return (
     <>
-     <div>
-      <h1><i>hello this my first project in react i'm {name}</i></h1>
-      <button onClick={greet}>click me</button>
+    
       <Navbar/>
-     </div>
+     
     </>
     
   )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,23 @@
 import "./Navbar.css";
+import logo from "./assets/logo.png";
 
 function Navbar(){
-    return <h1 className="nav">hello this is component</h1>;
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <img src={logo} alt="logo" />
+                <h2>Welcome Card React</h2>
+            </div>
+            <ul className="nav-link">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                
+            </ul>
+             <button className="nav-btn">Explore Cards</button>
+        </div>
+    );
 };
 
 export default Navbar;
